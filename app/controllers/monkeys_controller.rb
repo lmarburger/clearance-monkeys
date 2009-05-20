2656302de8f3206d29f76c282470c39276d6d6a8
Larry Marburger <larry@marburger.cc>
2009-05-19 22:31:28 -0400

    Rewrote the basic Monkey functional tests.

--- app/controllers/monkeys_controller.rb
+++ app/controllers/monkeys_controller.rb
@@ -1,85 +1,40 @@
 class MonkeysController < ApplicationController
-  # GET /monkeys
-  # GET /monkeys.xml
+
+  before_filter :load_monkey, :only => [ :show, :edit, :update, :destroy ]
+
   def index
     @monkeys = Monkey.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @monkeys }
-    end
   end
 
-  # GET /monkeys/1
-  # GET /monkeys/1.xml
   def show
     @monkey = Monkey.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @monkey }
-    end
   end
 
-  # GET /monkeys/new
-  # GET /monkeys/new.xml
   def new
     @monkey = Monkey.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @monkey }
-    end
   end
 
-  # GET /monkeys/1/edit
   def edit
-    @monkey = Monkey.find(params[:id])
   end
 
-  # POST /monkeys
-  # POST /monkeys.xml
   def create
-    @monkey = Monkey.new(params[:monkey])
-
-    respond_to do |format|
-      if @monkey.save
-        flash[:notice] = 'Monkey was successfully created.'
-        format.html { redirect_to(@monkey) }
-        format.xml  { render :xml => @monkey, :status => :created, :location => @monkey }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @monkey.errors, :status => :unprocessable_entity }
-      end
-    end
+    monkey = Monkey.create(params[:monkey])
+    redirect_to monkey
   end
 
-  # PUT /monkeys/1
-  # PUT /monkeys/1.xml
   def update
-    @monkey = Monkey.find(params[:id])
-
-    respond_to do |format|
-      if @monkey.update_attributes(params[:monkey])
-        flash[:notice] = 'Monkey was successfully updated.'
-        format.html { redirect_to(@monkey) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @monkey.errors, :status => :unprocessable_entity }
-      end
-    end
+    @monkey.update_attributes(params[:monkey])
+    redirect_to @monkey
   end
 
-  # DELETE /monkeys/1
-  # DELETE /monkeys/1.xml
   def destroy
-    @monkey = Monkey.find(params[:id])
     @monkey.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(monkeys_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to monkeys_url
   end
+
+  private
+
+    def load_monkey
+      @monkey = Monkey.find(params[:id])
+    end
 end
--- test/functional/monkeys_controller_test.rb
+++ test/functional/monkeys_controller_test.rb
@@ -1,45 +1,74 @@
 require 'test_helper'
 
 class MonkeysControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:monkeys)
+  context 'Given monkeys' do
+    setup { 3.times { Factory(:monkey) } }
+
+    context 'when viewing' do
+      setup { get :index }
+
+      should_assign_to(:monkeys) { Monkey.all }
+    end
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+  context 'Given a monkey' do
+    setup { @test_monkey = Factory(:monkey) }
 
-  test "should create monkey" do
-    assert_difference('Monkey.count') do
-      post :create, :monkey => { }
+    context 'when viewing' do
+      setup { get :show, :id => @test_monkey }
+      should_assign_to(:monkey) { @test_monkey }
     end
 
-    assert_redirected_to monkey_path(assigns(:monkey))
-  end
-
-  test "should show monkey" do
-    get :show, :id => monkeys(:one).to_param
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, :id => monkeys(:one).to_param
-    assert_response :success
-  end
-
-  test "should update monkey" do
-    put :update, :id => monkeys(:one).to_param, :monkey => { }
-    assert_redirected_to monkey_path(assigns(:monkey))
-  end
-
-  test "should destroy monkey" do
-    assert_difference('Monkey.count', -1) do
-      delete :destroy, :id => monkeys(:one).to_param
+    context 'when editing' do
+      setup { get :edit, :id => @test_monkey }
+      should_assign_to(:monkey) { @test_monkey }
     end
 
-    assert_redirected_to monkeys_path
+    context 'when deleting' do
+      setup { delete :destroy, :id => @test_monkey }
+
+      should_assign_to(:monkey) { @test_monkey }
+      should_change 'Monkey.count', :from => 1, :to => 0
+      should_redirect_to('the monkeys list') { monkeys_url }
+    end
+
+    context 'when updating' do
+      setup do
+        @data = Factory.attributes_for(:monkey)
+        put :update, :id => @test_monkey, :monkey => @data
+      end
+
+      should_assign_to(:monkey) { @test_monkey }
+      should_not_change 'Monkey.count'
+      should_redirect_to('the monkey') { monkey_url(@test_monkey) }
+
+      should 'update attributes' do
+        @test_monkey.reload
+
+        @test_monkey.name.should == @data[:name]
+        @test_monkey.age.should == @data[:age]
+      end
+    end
+  end
+
+  context 'Viewing the creation form' do
+    setup { get :new }
+    should_assign_to(:monkey)
+  end
+
+  context 'Creating a new monkey' do
+    setup do
+      @data = Factory.attributes_for(:monkey)
+      post :create, :monkey => @data
+
+      @new_monkey = Monkey.first
+    end
+
+    should_redirect_to('the monkey') { monkey_url(@new_monkey) }
+
+    should 'create attributes' do
+      @new_monkey.name.should == @data[:name]
+      @new_monkey.age.should == @data[:age]
+    end
   end
 end
